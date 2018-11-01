984afa9ff34e3fb0a594b226aa864348d1bb4d9f
lizardqueen98 <nadija.b@gmail.com>
2018-11-02 00:04:59 +0100

    bez grad klase

--- src/ba/unsa/etf/rpr/tutorijal03/Program.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -9,7 +9,7 @@ public class Program {
     public static void main(String[] args) {
         Imenik i=new Imenik();
         while(true) {
-            System.out.println("Unesite:\n 1-za dodavanje osobe,\n 2-pretrazivanje broja,\n 3-pretrazivanje osobe,\n 4-pretrazivanje osoba cije ime pocinje unesenim slovom,\n 5-spisak osoba iz datog grada,\n 6-spisak brojeva osoba iz datog grada, \n 7-za izlaz");
+            System.out.println("Unesite:\n 1-za dodavanje osobe,\n 2-pretrazivanje osobe,\n 3-pretrazivanje broja,\n 4-pretrazivanje osoba cije ime pocinje unesenim slovom,\n 5-spisak osoba iz datog grada,\n 6-spisak brojeva osoba iz datog grada, \n 7-za izlaz");
             Scanner ulaz = new Scanner(System.in);
             int n = ulaz.nextInt();
             ulaz.nextLine();
@@ -17,7 +17,7 @@ public class Program {
                 case 1:
                     System.out.println("Unesite ime:");
                     String ime = ulaz.nextLine();
-                    System.out.println("Unesite telefonski broj(formata:xxx/xxx-xxx");
+                    System.out.println("Unesite telefonski broj(formata:xxx/xxx-xxx)");
                     String broj = ulaz.nextLine();
                     if (broj.startsWith("06")) {
                         MobilniBroj mb = new MobilniBroj(Integer.valueOf(broj.substring(0, 3)), broj.substring(4, 11));
@@ -41,7 +41,7 @@ public class Program {
                     System.out.println(i.dajBroj(name));
                     break;
                 case 3:
-                    System.out.println("Unesite telefonski broj(formata:xxx/xxx-xxx");
+                    System.out.println("Unesite telefonski broj(formata:xxx/xxx-xxx)");
                     String br = ulaz.nextLine();
                     if (br.startsWith("06")) {
                         MobilniBroj mb = new MobilniBroj(Integer.valueOf(br.substring(0, 3)), br.substring(4, 11));
