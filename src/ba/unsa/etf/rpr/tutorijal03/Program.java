9ebcd2db839e71d544ab572431a1b8965ee85a05
lizardqueen98 <nadija.b@gmail.com>
2018-11-01 23:43:09 +0100

    bez grad klase

--- src/ba/unsa/etf/rpr/tutorijal03/Program.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Program.java
@@ -1,15 +1,42 @@
 package ba.unsa.etf.rpr.tutorijal03;
+import java.util.Scanner;
+
 import static ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad;
 import static ba.unsa.etf.rpr.tutorijal03.FiksniBroj.Grad.*;
 
 public class Program {
 
     public static void main(String[] args) {
-        Grad g = SARAJEVO;
-        System.out.println(g.getPozivni());
-        int a =5;
-        String s=String.valueOf(a);
-        System.out.println(s);
-
+        Imenik i=new Imenik();
+        System.out.println("Unesite:\n 1-za dodavanje osobe,\n 2-pretrazivanje broja,\n 3-pretrazivanje osobe,\n 4-pretrazivanje osoba cije ime pocinje unesenim slovom,\n 5-spisak osoba iz datog grada,\n 6-spisak brojeva osoba iz datog grada");
+        Scanner ulaz = new Scanner(System.in);
+        int n=ulaz.nextInt();
+        ulaz.nextLine();
+        switch (n){
+            case 1:
+                System.out.println("Unesite ime:");
+                String ime=ulaz.nextLine();
+                System.out.println("Unesite telefonski broj(formata:xxx/xxx-xxx");
+                String broj=ulaz.nextLine();
+                if(broj.startsWith("06")){
+                    MobilniBroj mb =new MobilniBroj(Integer.valueOf(broj.substring(0,3)),broj.substring(4,11));
+                    i.dodaj(ime,mb);
+                }
+                else if(broj.startsWith("+")){
+                    MedunarodniBroj mb=new MedunarodniBroj(broj.substring(0,6),broj.substring(7,14));
+                    i.dodaj(ime,mb);
+                }
+                else{
+                    for(Grad g:Grad.values()){
+                        if(g.getPozivni().equals(broj.substring(0,3))){
+                            FiksniBroj fb=new FiksniBroj(g,broj.substring(4,11));
+                            i.dodaj(ime,fb);
+                            break;
+                        }
+                    }
+                }
+                break;
+            case 2:
+        }
     }
 }
