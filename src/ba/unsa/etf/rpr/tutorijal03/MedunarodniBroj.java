03aabce3964c1fd5040d5629f942f1a604eb163d
lizardqueen98 <nadija.b@gmail.com>
2018-11-01 10:53:25 +0100

    TelefonskiBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MedunarodniBroj extends TelefonskiBroj{
+    private String drzava,broj;
+    public MedunarodniBroj(String drzava, String broj){
+        this.drzava=drzava;
+        this.broj=broj;
+    }
+    @Override
+    public String ispisi(){
+
+    }
+    @Override
+    public int hashCode(){
+
+    }
+}
