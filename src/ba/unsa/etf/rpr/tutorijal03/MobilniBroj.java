319c20598762dadd497dcafeb529eda3b3965f89
lizardqueen98 <nadija.b@gmail.com>
2018-11-01 10:26:25 +0100

    TelefonskiBroj

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,19 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj extends TelefonskiBroj{
+    private int mobilnaMreza;
+    private String broj;
+    public MobilniBroj(int mobilnaMreza, String broj){
+        this.mobilnaMreza=mobilnaMreza;
+        this.broj=broj;
+    }
+    @Override
+    public String ispisi(){
+        
+    }
+    @Override
+    public int hashCode(){
+
+    }
+};
+
